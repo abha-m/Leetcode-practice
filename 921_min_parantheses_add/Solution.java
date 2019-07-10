28cce04d9d7663cff61b0a8e1f2cf64e499150fb
abha-m <abhamutalik@gmail.com>
2019-07-10 20:48:22 +0530

    valid parantheses solution 2

--- 921_min_parantheses_add/Solution.java
+++ 921_min_parantheses_add/Solution.java
@@ -27,12 +27,34 @@ class Solution {
         return st.size()+len_array;
     }
 
+    // Solution 2
+    public int minAddToMakeValid2(String S) {
+        int count_open = 0;
+        int count_closed = 0;
+        if(S== null) 
+            return 0;
+        for (int i=0; i<S.length(); i++) {
+            char c = S.charAt(i);
+            if(c == '(') {
+                count_open++;
+            }
+            else {
+                if(count_open > 0) {
+                    count_open--;
+                }
+                else {
+                    count_closed++;
+                }
+            }
+        }
+        return count_open+count_closed;
+    }
     
 
     public static void main(String args[]) {
         Solution s = new Solution();
         System.out.println(s.minAddToMakeValid1("()))(("));
-        
+        System.out.println(s.minAddToMakeValid2("()))(("));
     }
 
 
