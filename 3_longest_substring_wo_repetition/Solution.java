14a5996056784f35c9f231a220c7fb0cc79bf3d1
abha-m <abhamutalik@gmail.com>
2019-07-14 16:25:39 +0530

    longest substring without repeated chars : added the hashset approach

--- 3_longest_substring_wo_repetition/Solution.java
+++ 3_longest_substring_wo_repetition/Solution.java
@@ -5,26 +5,42 @@ class Solution {
         if(s==null) {
             return 0;
         }
-        int i = 0;
-        List<Character> al = new ArrayList<Character>(); 
-        int max = 0;
-        while(i < s.length()) {
-            char c = s.charAt(i);
-            if(al.contains(c)) {
-                int j = 0;
-                while(j <= al.indexOf(c)) {
-                    al.remove(j);
-                }   
+        // Approach 1
+        // int i = 0;
+        // List<Character> al = new ArrayList<Character>(); 
+        // int max = 0;
+        // while(i < s.length()) {
+        //     char c = s.charAt(i);
+        //     if(al.contains(c)) {
+        //         int j = 0;
+        //         while(j <= al.indexOf(c)) {
+        //             al.remove(j);
+        //         }   
+        //     }
+        //     al.add(c);
+        //     max = (al.size() > max) ? al.size() : max;
+        //     i++;
+        // }
+
+        // Approach 2
+        Set<Character> hs = new HashSet<Character>();
+        int i=0, j=0, max=0;
+        while(i<s.length() && j<s.length()) {
+            if(hs.add(s.charAt(j))) {
+                max = (j-i+1)>max ? (j-i+1) : max;
+                j++;
             }
-            al.add(c);
-            max = (al.size() > max) ? al.size() : max;
-            i++;
+            else {
+                hs.remove(s.charAt(i));
+                i++;
+                max = (j-i+1)>max ? (j-i+1) : max;
+            }
         }
         return max;
     }
 
     public static void main(String args[]) {
         Solution s = new Solution();
-        System.out.println(s.lengthOfLongestSubstring(""));
+        System.out.println(s.lengthOfLongestSubstring("abcabcbb"));
     }
 }
